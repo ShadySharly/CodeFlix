b9545eb8d2a3f564c34a426d91e75d6a0eeaaca9
sebastianorellanav <sebastian.orellana.v@usach.cl>
2021-06-29 14:21:47 -0400

    se agrega vista home

--- frontend-codeflix/src/App.vue
+++ frontend-codeflix/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <navbar></navbar>
-  <router-view></router-view>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -17,27 +17,3 @@ export default {
   }),
 };
 </script>
-
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- frontend-codeflix/src/views/Home.vue
+++ frontend-codeflix/src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <b-container class="container-general mx-auto mt-3 p-5">
+       <b-row align-h="center" class="pt-0 pb-3">
+        <b-col class="col-md-3 col-7 pt-0" >
+          <b-img :src="require('../assets/codeflix-icon.gif')" fluid alt="Fluid image"></b-img>
+        </b-col>
+        <b-col class="col-6 col-md-3" align-self="end">
+          <div class="d-flex">
+            <h1>
+            CodeFlix
+          </h1>
+          <h6>
+            Search
+          </h6>
+          </div>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="12">
+          <b-form-input placeholder="¿Qué estas buscando?"></b-form-input>
+        </b-col>
+      </b-row>
+      <b-row align-h="center py-4 mt-2">
+        <b-col class="col-6 col-md-3" >
+          <b-button class="btn-home-buscar">Buscar</b-button>
+        </b-col>
+        <b-col class="col-6 col-md-3">
+          <b-button class="btn-home-explorar">¿No sabes que buscar?</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+
+    components: {
+    },
   }
-}
 </script>
+
+<style scoped>
+.section-home{
+  background-color: cornflowerblue;
+}
+
+.btn-home-buscar{
+  background-color: coral;
+  border-color: coral;
+  width: 100%;
+}
+
+.btn-home-explorar{
+  background-color: #FFC463;
+  border-color: #FFC463;
+  width: 100%;
+  color: #2D2E40;
+}
+
+.container-general{
+   background: rgb(226,226,226);
+background: linear-gradient(180deg, rgba(226,226,226,1) 0%, rgba(255,255,255,1) 100%); 
+}
+</style>
